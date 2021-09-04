c6c2fdacc13f868823ff44f42358b0c36eaa5528
johwiyong <miseonghwi@gmail.com>
2021-09-04 21:44:03 +0900

    modified

--- code/appendix/training_raw_adding_checkpoint/lr_schedule.py
+++ code/appendix/training_raw_adding_checkpoint/lr_schedule.py
@@ -1,12 +1,15 @@
 import tensorflow as tf
 
 class LRSchedule(tf.keras.optimizers.schedules.LearningRateSchedule):
-    def __init__(self, init_lr, warmup_epoch,
-                 decay_fn
+    def __init__(self, init_lr, warmup_epoch, steps_per_epoch,
+                 decay_fn, *,
+                 continue_epoch = 1
                  ):
         self.init_lr = init_lr
         self.decay_fn = decay_fn
         self.warmup_epoch = warmup_epoch
+        self.steps_per_epoch = steps_per_epoch
+        self.continue_epoch = continue_epoch
         self.lr = 1e-4
         
     def get_config(self):
@@ -19,7 +22,9 @@ class LRSchedule(tf.keras.optimizers.schedules.LearningRateSchedule):
     
     # No Override
     def on_epoch_begin(self, epoch, logs = None):
-        global_epoch = tf.cast(epoch + 1, tf.float64)
+        epoch = tf.cast(epoch, tf.float64)
+        
+        global_epoch = tf.cast(epoch, tf.float64)
         warmup_epoch_float = tf.cast(self.warmup_epoch, tf.float64)
         
         lr = tf.cond(
@@ -31,6 +36,12 @@ class LRSchedule(tf.keras.optimizers.schedules.LearningRateSchedule):
         self.lr = lr
         
     def __call__(self, step):
-        self.on_epoch_begin(step, logs = None)
-        
+        def compute_epoch(step):
+            return step // self.steps_per_epoch
+
+        epoch = compute_epoch(step)
+        epoch = epoch + self.continue_epoch
+
+        self.on_epoch_begin(epoch, logs = None)
+
         return self.lr
